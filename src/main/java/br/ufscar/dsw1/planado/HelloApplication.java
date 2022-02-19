aa0858d21c566922434bd64d9709d90bcc711347
gabrielpenajo <gabrielpenajo13@gmail.com>
2022-02-19 20:35:55 -0300

    docker-compose up'd

---
+++ src/main/java/br/ufscar/dsw1/planado/HelloApplication.java
@@ -0,0 +1,9 @@
+package br.ufscar.dsw1.planado;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
